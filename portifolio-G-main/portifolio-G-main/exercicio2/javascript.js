faf61457312fcabc22cd6a94f3a5773f3f8dac16
lilg777-boop <a.gabriel11@escola.pr.gov.br>
2025-05-16 09:10:02 -0300

    Add files via upload

---
+++ portifolio-G-main/portifolio-G-main/exercicio2/javascript.js
@@ -0,0 +1,21 @@
+function calculo (){
+
+    let anoNascimento = 0;
+    let idade = 0;
+
+    anoNascimento = Number (window.prompt( "digite o ano atual"));
+    idade = 2025 - anoNascimento;
+
+
+
+    alert("Sua Idade é" + idade);
+
+
+    if(idade > 18){
+        alert("Você pode tirar carteira de motorista");
+
+    } else {
+        let dif = 18 - idade;
+        alert("Você não pode tirar carteira de motorista pois ainda faltam" + dif +" anos para completar 18 anos");
+    }
+}
